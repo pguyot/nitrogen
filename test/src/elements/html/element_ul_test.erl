137a702d3fd7f9968049cb91cca1a1a8e23fcf76
Paul Guyot <paul@coriander.local>
2010-01-07 16:47:27 +0100

    Additional ul test

--- test/src/elements/html/element_ul_test.erl
+++ test/src/elements/html/element_ul_test.erl
@@ -17,11 +17,16 @@ new_ul_3() ->
   Rec_ul = #ul{class="t_ul", style="color: cyan;", body="SOME BODY"},
   lists:flatten(element_ul:render("3",Rec_ul)).
 
+new_ul_4() ->
+  Rec_ul = #ul{class="t_ul", style="color: cyan;", body=[#li{body="li_1"}, #li{body="li_2"}]},
+  lists:flatten(element_ul:render("4",Rec_ul)).
+
 basic_test_() ->
   [
    ?_assertEqual("<ul id=\"1\" class=\"list\"/>",new_ul_1()),
    ?_assertEqual("<ul id=\"2\" class=\"list t_ul\"/>",new_ul_2()),
    ?_assertEqual("<ul id=\"3\" class=\"list t_ul\" style=\"color: cyan;\">SOME BODY</ul>",new_ul_3()),
+   ?_assert(eunit_helper:regexpMatch("<ul id=\"4\" class=\"list t_ul\" style=\"color: cyan;\"><li id=\"temp.*?\" class=\"listitem\">li_1</li><li id=\"temp.*?\" class=\"listitem\">li_2</li></ul>",new_ul_4())),
    ?_assertEqual([module,id,actions,show_if,class,style,title,attrs,body],
 	 element_ul:reflect())
   ].
